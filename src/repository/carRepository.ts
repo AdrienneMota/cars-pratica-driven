641b76452320d7a297519b61b282014f8dfc363f
AdrienneMota <adriennemota97@gmail.com>
2023-01-25 19:28:40 -0400

    first commit

---
+++ src/repository/carRepository.ts
@@ -0,0 +1,46 @@
+import prisma from "../config/database.js";
+
+async function getCars() {
+  return prisma.cars.findMany();
+}
+
+async function getCar(id: number) {
+  return prisma.cars.findFirst({
+    where: { id: id}
+  })
+}
+
+async function getCarWithLicensePlate(licensePlate: string) {
+  return prisma.cars.findFirst({
+    where: { licensePlate: licensePlate}
+  })
+}
+
+async function createCar(model: string, licensePlate: string, year: string, color: string) {
+  return await prisma.cars.create({
+    data: {
+      model,
+      licensePlate,
+      year,
+      color
+    }
+  })
+}
+
+async function deleteCar(id: number) {
+  await prisma.cars.delete({
+    where: {
+      id: id
+    }
+  });
+}
+
+const carRepository = {
+  getCar,
+  getCarWithLicensePlate,
+  getCars,
+  createCar,
+  deleteCar
+}
+
+export default carRepository;
